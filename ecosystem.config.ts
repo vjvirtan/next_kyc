b973ee6fc1935dc2e81ac03e026b4b9bc99a3772
Ville Virtanen <vjvirtan@gmail.com>
2024-10-24 19:41:47 +0300

    Styling and fixes

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -2,11 +2,24 @@ module.exports = {
   apps: [
     {
       name: 'kyc-api',
-      script: 'npm',
-      args: 'start',
+      script: './kyc-api.js', // Path to your script
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
       }
     }
-  ]
-}
+  ],
+  deploy : {
+    production : {
+      user : 'firehay',
+      host : 'kyc.firehay.com',
+   //   ref  : 'origin/main',
+      repo : 'git@github.com:vjvirtan/next_kyc',
+      path : '/home/firehay/Kyc-Api',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
+
