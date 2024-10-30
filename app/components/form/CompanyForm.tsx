769bf6bb67149e87fda9912fba9f8dd40918255b
Ville Virtanen <vjvirtan@gmail.com>
2024-10-30 15:36:48 +0200

    Bug overround.

--- app/components/form/CompanyForm.tsx
+++ app/components/form/CompanyForm.tsx
@@ -20,15 +20,7 @@ const CompanyForm = () => {
   };
   return (
     <>
-      <div>{translatePageText("findCompany")} </div>
-      <div className='border-spacing-2 border-green-300'>
-        <input
-          className=''
-          id='businessId'
-          onChange={(e) => findCompany(e.target.value)}
-          type='text'
-        ></input>
-      </div>
+      <div className='border-spacing-2 border-green-300'></div>
       <br />
       <div
         className='text-sm'
@@ -38,9 +30,24 @@ const CompanyForm = () => {
         }}
       >
         <div>{translatePageText("exampleCompanyIDs")}</div>
-        <div id='7777777-1'>"7777777-1"</div>
-        <div id='8888888-1'>"8888888-1"</div>
-        <div id='9999999-1'>"9999999-1"</div>
+        <button
+          className='bg-base-mid text-white border-2 rounded-md border-base-dark shadow-sm shadow-gray-400 m-2 p-4 hover:border-white hover:bg-blue-500'
+          id='7777777-1'
+        >
+          "7777777-1"
+        </button>
+        <button
+          className='bg-base-mid text-white border-2 rounded-md border-base-dark shadow-sm shadow-gray-400  m-2 p-4 hover:border-white hover:bg-blue-500'
+          id='8888888-1'
+        >
+          "8888888-1"
+        </button>
+        <button
+          className='bg-base-mid text-white border-2 rounded-md border-base-dark shadow-sm shadow-gray-400  m-2 p-4 hover:border-white hover:bg-blue-500'
+          id='9999999-1'
+        >
+          "9999999-1"
+        </button>
       </div>
     </>
   );
